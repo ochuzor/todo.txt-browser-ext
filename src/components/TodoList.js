a644c4b710e278fd53f42d82b2a8e9b7892a4541
Chinedu Eze <60477922+ochuzor@users.noreply.github.com>
2020-04-01 08:35:47 +0300

    setup item delete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,11 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 
 export default function({todos}) {
+    const onTodoDelete = (todo) => {
+        console.log(todo);
+    };
+
     return (<div>
-        {todos.map((todo) => <TodoListItem key={todo.id} todo={todo}/>)}
+        {todos.map((todo) => <TodoListItem key={todo.id} todo={todo} onTodoDelete={onTodoDelete} />)}
     </div>);
 }
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default function TodoListItem({todo}) {
+export default function TodoListItem({todo, onTodoDelete}) {
+    const deleteTodoHandler = () => {
+        if (window.confirm('are you sure you want to delete')) {
+            onTodoDelete(todo);
+        }
+    };
+
     return (<div className="todo-list-item">
         {todo.text}
         <div>
-            <button>delete</button>
+            <button onClick={deleteTodoHandler}>delete</button>
             <button>edit</button>
         </div>
     </div>)
